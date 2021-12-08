5468dd68fd316d2c4a5f475bb10bb9a027d9ac0d
itkimura <itkimura@student.hive.fi>
2021-12-08 15:36:54 +0200

    First commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,68 @@
 /*   By: itkimura <itkimura@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 23:40:12 by itkimura          #+#    #+#             */
-/*   Updated: 2021/12/06 23:44:24 by itkimura         ###   ########.fr       */
+/*   Updated: 2021/12/07 16:28:09 by itkimura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int get_next_line(const int fd, char **line)
+#include "get_next_line.h"
+#include <stdio.h>
+
+char	*read_file(const int fd, char **line)
 {
+	char	*buff;
+	int		byte;
+
+	buff = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
+	if (!buff)
+		return (0);
+	byte = 1;
+	while (ft_strchr(, '\n') != 0 && byte)
+	{
+		byte = read (fd, &buff, BUFF_SIZE);
+		if (byte == NULL)
+		{
+			free(buff);
+			return (0);
+		}
+		buff[byte] = '\0';
+	}
+	return (buff);
+}
+
+int	get_next_line(const int fd, char **line)
+{
+
+	if (fd < 0 || BUFF_SIZE <= 0)
+		return (0);
+	line = read_file(fd, line);
+	if (str == NULL)
+		return (0);
+	return (1);
+}
+
+int main(int argc, char *argv[])
+{
+	char *file_name;
+	if (argc == 1)
+	{
+		file_name = "test.txt";
+	}
+    else
+        file_name = argv[1];
+    int fd = open(file_name, O_RDONLY);
+    // int fd = 999999;
+    char *line;
+    int status;
+
+    printf("\n[START] %s\n\tfd: %d\n\n", file_name, fd);
+    do{
+        status = get_next_line(fd, &line);
+        printf("status:  \t%d\n", status);
+        printf("read txt:\t%s\n", line);
+        free(line);
+    }while (status == 1);
+    printf("\n[FINISH] %s\n\tstatus: %d\n\n", file_name, status);
+
+    close(fd);
 }
---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itkimura <itkimura@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 13:57:04 by itkimura          #+#    #+#             */
+/*   Updated: 2021/12/07 15:50:21 by itkimura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 13
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+char	*ft_strchr(const char *s, int c);
+
+#endif
